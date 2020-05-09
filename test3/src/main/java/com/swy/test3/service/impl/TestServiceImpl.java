f21c1e0dc5f8f3716260a4fe99888a611543a597
Meryl <aa5151521@126.com>
2020-05-09 14:57:00 +0800

    update

--- test3/src/main/java/com/swy/test3/service/impl/TestServiceImpl.java
+++ test3/src/main/java/com/swy/test3/service/impl/TestServiceImpl.java
@@ -4,10 +4,12 @@ import com.jds.core.common.BizException;
 import com.jds.core.service.impl.BaseServiceImpl;
 import com.swy.test3.service.TestService;
 import io.seata.core.context.RootContext;
+import org.apache.poi.sl.draw.geom.Guide;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class TestServiceImpl extends BaseServiceImpl implements TestService {
@@ -16,12 +18,18 @@ public class TestServiceImpl extends BaseServiceImpl implements TestService {
 //    @Transactional
     public void txTest3() {
         Map map = new HashMap<>();
+//        map.put("id", UUID.randomUUID().toString());
+        map.put("gid", UUID.randomUUID().toString());
         map.put("name", "meryl");
         map.put("type", "hello");
         db("temp").insert(map);
 
         String xid = RootContext.getXID();
         System.out.println("===============XID:" + xid + "==================");
+
+        Map test= new HashMap();
+        test.put("cont","ddd");
+        db("test").insert(test);
 
 
 //        if (1 == 1) throw new BizException("errdddd test3");
